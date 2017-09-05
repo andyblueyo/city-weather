767af03b6c153a33b2a80c32ba37ea198130e975
andyblueyo <abchen@uw.edu>
2017-09-05 04:48:07 -0700

    fix axes labels

--- app.R
+++ app.R
@@ -8,6 +8,7 @@ ui <- fluidPage(
   titlePanel("City Weather", windowTitle = "AH NOPE"),
   sidebarLayout(
     sidebarPanel(
+      selectInput("type", "Temp Type", choices = c("1", "2", "3"), selected = "2", multiple = TRUE),
       dateRangeInput(inputId = "date", label = "Date Range", start = "2014-10-1", end = "2015-6-8", min = "2014-10-1", max = "2015-6-9"),
       sliderInput(inputId = "temp", label = "Temp Range", min = 0, max = 100, value = c(25,75))
     ),
@@ -25,7 +26,13 @@ server <- function(input, output) {
     weather %>% filter(actual_mean_temp > input$temp[1]) %>% filter(actual_mean_temp < input$temp[2])
   })
   output$tempplot <- renderPlotly({
-    plot_ly(temp.input(), x = ~actual_mean_temp, y = ~actual_min_temp)
+    x <- list(
+      title = "Actual Mean Temp"
+    )
+    y <- list(
+      title = "Actual Min Temp"
+    )
+    plot_ly(temp.input(), x = ~actual_mean_temp, y = ~actual_min_temp) %>% layout(xaxis = x, yaxis = y)
   })
   output$temptable <- renderTable({
     temp.input()

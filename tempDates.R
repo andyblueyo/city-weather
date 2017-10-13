9bf9804cb0eb79e0d21e772ac37b10dd9ea24a60
andyblueyo <abchen@uw.edu>
2017-10-12 21:43:11 -0700

    add temp by dates

--- tempDates.R
+++ tempDates.R
@@ -2,6 +2,7 @@ library(dplyr)
 location <- read.csv("data/location.csv", stringsAsFactors = FALSE)
 files <- location$file_name
 
+
 charDate <- function(csv){
   csv <- read.csv(paste0("data/",csv,".csv"), stringsAsFactors = FALSE)
   csv$date <- as.Date(csv$date, "%Y-%m-%d")
@@ -10,18 +11,23 @@ charDate <- function(csv){
 
 list.data <- lapply(files, charDate)
 
+#read in all the files
 for (i in seq(list.data)) {
   assign(paste(files[i], sep = ""), list.data[[i]])
 }
   
-
-
-
-tempDates <- function(cal.date){
-  kcltTemp <- kclt %>% filter(date == cal.date)
-  print(kcltTemp$actual_mean_temp)
-  
-  return(4)
+for (i in seq(list.data)) {
+  list.data[[i]] <- list.data[[i]] %>% filter(list.data[[i]]$date == "2014-07-06")
+  #assign(paste(files[i], sep = ""), list.data[[i]])
+  location$actual_mean_temp[location$file_name == files[i]] <- list.data[[i]]$actual_mean_temp
 }
 
-tempDates("2014-07-01")
+location$actual_mean_temp[location$file_name == "KCLT"] <- KCLT[["actual_mean_temp"]]
+
+tempDates <- function(cal.date){
+  kcltTemp <- KCLT %>% filter(date == cal.date)
+  print(kcltTemp)
+
+}
+
+tempDates("2014-07-06")

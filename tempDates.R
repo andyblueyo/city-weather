ac4ae27d28ff64419bc74d51d9a5c18207b08eb9
andyblueyo <abchen@uw.edu>
2017-11-01 21:14:12 -0700

    add comments for tempDates

--- tempDates.R
+++ tempDates.R
@@ -1,10 +1,12 @@
 library(dplyr)
 
 tempDates <- function(cal.date){
+  # read in the file names, can pull this out
   location <- read.csv("data/location.csv", stringsAsFactors = FALSE)
   files <- location$file_name
   
   # convert csv files with appropriate dates
+  # also pull this out
   charDate <- function(csv){
     csv <- read.csv(paste0("data/",csv,".csv"), stringsAsFactors = FALSE)
     csv$date <- as.Date(csv$date, "%Y-%m-%d")
@@ -16,6 +18,8 @@ tempDates <- function(cal.date){
     list.data[[i]] <- list.data[[i]] %>% filter(list.data[[i]]$date == cal.date)
     location$actual_mean_temp[location$file_name == files[i]] <<- list.data[[i]]$actual_mean_temp
   }
-
+  
 }
+
+tempDates("2014-07-09")
 
